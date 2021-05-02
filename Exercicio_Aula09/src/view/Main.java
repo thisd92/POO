b124cb4f4ad465de74a0b9af4de7d96690cb7545
thisd92 <80495413+thisd92@users.noreply.github.com>
2021-05-02 11:17:31 -0300

    Ex Aula 09 FINALIZADO

--- Exercicio_Aula09/src/view/Main.java
+++ Exercicio_Aula09/src/view/Main.java
@@ -12,42 +12,42 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Circulo areaCirculo = new Circulo();
-        Quadrado areaQuadrado = new Quadrado();
-        Retangulo areaRetangulo = new Retangulo();
+        Circulo circulo = new Circulo();
+        Quadrado quadrado = new Quadrado();
+        Retangulo retangulo = new Retangulo();
         
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Digite o raio do circulo: ");
         double raio = sc.nextDouble();
         
-        areaCirculo.setRaio(raio);
+        circulo.setRaio(raio);
         
-        System.out.println("O valor da area do circulo e: " + areaCirculo.area() + "\n");
-        System.out.println("O valor do perimetro do circulo e: " + areaCirculo.perimetro()+ "\n");
+        System.out.println("O valor da area do circulo e: " + circulo.area() + "\n");
+        System.out.println("O valor do perimetro do circulo e: " + circulo.perimetro()+ "\n");
         
         System.out.println("------------------------------------");
         
         System.out.println("Digite o valor do lado do quadrado: ");
         double ladoQuadrado = sc.nextDouble();
         
-        areaQuadrado.setLado(ladoQuadrado);
+        quadrado.setLado(ladoQuadrado);
         
-        System.out.println("O valor da area do quadrado e: " + areaQuadrado.area() + "\n");
-        System.out.println("O valor do perimetro do quadrado e: " + areaQuadrado.perimetro()+ "\n");
+        System.out.println("O valor da area do quadrado e: " + quadrado.area() + "\n");
+        System.out.println("O valor do perimetro do quadrado e: " + quadrado.perimetro()+ "\n");
 
         System.out.println("------------------------------------");
         
         System.out.println("Digite o valor do lado do retangulo: ");
         double ladoRetangulo = sc.nextDouble();
-        areaRetangulo.setLado(ladoRetangulo);
+        retangulo.setLado(ladoRetangulo);
         
         System.out.println("Digite o valor da base do retangulo: ");
         double baseRetangulo = sc.nextDouble();
-        areaRetangulo.setBase(baseRetangulo);
+        retangulo.setBase(baseRetangulo);
                
-        System.out.println("O valor da area do retangulo e: " + areaRetangulo.area() + "\n");
-        System.out.println("O valor do perimetro do retangulo e: " + areaRetangulo.perimetro()+ "\n");        
+        System.out.println("O valor da area do retangulo e: " + retangulo.area() + "\n");
+        System.out.println("O valor do perimetro do retangulo e: " + retangulo.perimetro()+ "\n");        
     }
     
     
